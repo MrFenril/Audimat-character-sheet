93efe2694f21802e53674684934cc741b49cd46f
Fenril <rigolat.sebastien@gmail.com>
2023-07-19 13:51:25 +0200

    add persisency

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-let clearStorageButton = undefined;
-let client = {
+let clearStorageButton = null;
+let client = null;
+let defaultData = {
     name: '',
     class: '',
-    health: 0,
     armor: 0,
+    health: 0,
     dices: 0,
     stats: {
         mental: 0,
@@ -11,6 +12,7 @@ let client = {
         social: 0
     },
     weaknesses: '',
+    qualities: '',
     relationship: '',
     backstory: '',
     inventory: [],
@@ -29,9 +31,9 @@ async function initSheet() {
     try {
         const campaignInfo = await TS.campaigns.getMoreInfoAboutCurrentCampaign()
         const cacheData = await TS.localStorage.campaign.getBlob()
-        TS.debug.log("-------cache: " + cacheData);
         if (!cacheData) {
             TS.debug.log("Empty cache for current campaign, initialise data");
+
             const playerClient = await TS.clients.whoAmI()
 
             // Potentially usefull later
@@ -41,88 +43,77 @@ async function initSheet() {
             // const creatureInfo = await TS.creatures.getMoreInfo(["40411f31-9049-4bd3-8993-a75cdb87661f"])
 
             client = {
-                ...client,
+                ...defaultData,
                 clientInfo: playerClient,
             }
             TS.localStorage.campaign.setBlob(JSON.stringify(client));
         }
-        else client = JSON.parse(cacheData);
+        else {
+            client = JSON.parse(cacheData);
+            TS.debug.log("-------cache: " + JSON.stringify(client, null, 4));
+        }
+
+        for (statName of Object.keys(client.stats)) {
+            const $el = document.getElementById(`character-${statName}`)
+            $el.value = client.stats[statName];
+        }
+
+        document.getElementById(`character-name`).value = client.name;
+        document.getElementById(`character-class`).value = client.class;
+        document.getElementById(`character-health`).value = client.health;
+        document.getElementById(`character-armor`).value = client.armor;
+        document.getElementById(`character-dices`).value = client.dices;
+
+        document.getElementById(`weaknesses`).value = client.weaknesses;
+        document.getElementById(`qualities`).value = client.qualities;
+        document.getElementById(`relations`).value = client.relationship;
+        document.getElementById(`backstory`).value = client.backstory;
+
     } catch (e) {
         TS.debug.log("error: " + e)
     }
 }
 
-function onInputChange(input) {
-    //handles input changes to store them in local storage
+function OnValueChange(field, value) {
+    field = value;
+    saveSheet();
+}
 
-    let data;
-    // get already stored data
-    TS.localStorage.campaign.getBlob().then((storedData) => {
-        //parse stored blob as json, but also handle if it's empty by
-        //defaulting to an empty json document "{}" if stored data is false
-        data = JSON.parse(storedData || "{}");
-        if (input.type == "checkbox") {
-            data[input.id] = input.checked ? "on" : "off";
-        } else {
-            data[input.id] = input.value;
+async function saveSheet() {
+    TS.debug.log("Saving to cache: " + JSON.stringify(client, null, 4));
+    await TS.localStorage.campaign.setBlob(JSON.stringify(client));
+}
+
+async function clearSheet() {
+    try {
+        // clear stored data
+        await TS.localStorage.campaign.deleteBlob()
+
+        // clear sheet inputs
+        let inputs = document.querySelectorAll("input,textarea");
+        inputs.forEach(input => input.value = '');
+        for (let input of inputs) {
+            if (input.type == 'number') input.value = 0;
+            else input.value = "";
         }
-        //set new data, handle response
-        TS.localStorage.campaign.setBlob(JSON.stringify(data)).then(() => {
-            //if storing the data succeeded, enable the clear storage button
-            clearStorageButton.classList.add("danger");
-            clearStorageButton.disabled = false;
-            clearStorageButton.textContent = "Clear Character Sheet";
-            TS.debug.log("set in storage: " + (JSON.stringify(data)));
-        }).catch((setBlobResponse) => {
-            TS.debug.log("Failed to store change to local storage: " + setBlobResponse.cause);
-            console.error("Failed to store change to local storage:", setBlobResponse);
-        });
-    }).catch((getBlobResponse) => {
-        TS.debug.log("Failed to load data from local storage: " + getBlobResponse.cause);
-        console.error("Failed to load data from local storage:", getBlobResponse);
-    });
+        saveSheet();
 
-    if (input.id == "abilities-text") {
-        let actions = parseActions(input.value);
-        addActions(actions);
+        // clear skills
+        // clear capacities
+    } catch (error) {
+        TS.debug.log("Failed to delete local storage: " + error.cause);
+        console.error("Failed to delete local storage:", error);
     }
 }
 
-function clearSheet() {
-    //clear stored data
-    TS.localStorage.campaign.deleteBlob().then(() => {
-        //if the delete succeeded (.then), set the UI to reflect that
-        clearStorageButton.classList.remove("danger");
-        clearStorageButton.textContent = "Character Sheet Empty";
-    }).catch((deleteResponse) => {
-        //if the delete failed (.catch), write a message to symbiote log
-        TS.debug.log("Failed to delete local storage: " + deleteResponse.cause);
-        console.error("Failed to delete local storage:", deleteResponse);
-    });
-
-    //clear sheet inputs
-    let inputs = document.querySelectorAll("input,textarea");
-    for (let input of inputs) {
-        switch (input.type) {
-            case "button":
-                break;
-            case "checkbox":
-                input.checked = false;
-                break;
-            default:
-                input.value = "";
-                break;
-        }
-    }
-}
-
-function onStateChangeEvent(msg) {
+async function onStateChangeEvent(msg) {
     if (msg.kind === "hasInitialized") {
         TS.debug.log("Init")
         //the TS Symbiote API has initialized and we can begin the setup. think of this as "init".
         clearStorageButton = document.getElementById("clear-storage");
-        initSheet();
+        await initSheet();
         initTabs();
         initSkillsTables();
+        InitCapacities();
     }
 }
